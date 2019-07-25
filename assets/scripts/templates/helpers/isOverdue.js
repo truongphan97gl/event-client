2708f5c07a20dfa0fdffe441ff5a1c3e8594416f
truongphan97gl <truongphan97gl@gmail.com>
2019-07-25 17:48:23 -0400

    Fixing Overdue.js

--- assets/scripts/templates/helpers/isOverdue.js
+++ assets/scripts/templates/helpers/isOverdue.js
@@ -1,18 +1,20 @@
 'use strict'
 const isOverdue = (date) => {
-  const today = new Date()
-  const toDate = today.getFullYear() + '-' + (
-    today.getMonth() + 1) + '-' + today.getDate()
-  const dateArray = date.split('-')
-  const toDateArray = toDate.split('-')
-  if (Number(toDateArray[0]) > Number(dateArray[0])) {
-    return true
-  } else if (Number(toDateArray[0]) === Number(dateArray[0])) {
-    if (Number(toDateArray[1]) > Number(dateArray[1])) {
+  if (date !== null) {
+    const today = new Date()
+    const toDate = today.getFullYear() + '-' + (
+      today.getMonth() + 1) + '-' + today.getDate()
+    const dateArray = date.split('-')
+    const toDateArray = toDate.split('-')
+    if (Number(toDateArray[0]) > Number(dateArray[0])) {
       return true
-    } else if (Number(toDateArray[1]) === Number(dateArray[1])) {
-      if (Number(toDateArray[2]) > Number(dateArray[2])) {
+    } else if (Number(toDateArray[0]) === Number(dateArray[0])) {
+      if (Number(toDateArray[1]) > Number(dateArray[1])) {
         return true
+      } else if (Number(toDateArray[1]) === Number(dateArray[1])) {
+        if (Number(toDateArray[2]) > Number(dateArray[2])) {
+          return true
+        }
       }
     }
   }
