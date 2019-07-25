dc9c9d72a36e8b68678531a2ccef69540695aac6
truongphan97gl <truongphan97gl@gmail.com>
2019-07-25 17:29:46 -0400

    Removing console

--- assets/scripts/templates/helpers/isOverdue.js
+++ assets/scripts/templates/helpers/isOverdue.js
@@ -5,20 +5,13 @@ const isOverdue = (date) => {
     today.getMonth() + 1) + '-' + today.getDate()
   const dateArray = date.split('-')
   const toDateArray = toDate.split('-')
-  console.log(dateArray)
-  console.log(toDateArray)
-  console.log(Number(toDateArray[0]) > Number(dateArray[0]))
-  console.log(Number(toDateArray[0]))
-  console.log(Number(dateArray[0]))
   if (Number(toDateArray[0]) > Number(dateArray[0])) {
     return true
   } else if (Number(toDateArray[0]) === Number(dateArray[0])) {
     if (Number(toDateArray[1]) > Number(dateArray[1])) {
-      console.log('second')
       return true
     } else if (Number(toDateArray[1]) === Number(dateArray[1])) {
       if (Number(toDateArray[2]) > Number(dateArray[2])) {
-        console.log('third')
         return true
       }
     }
